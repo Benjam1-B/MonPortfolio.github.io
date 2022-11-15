f7f9db110c5fc6537b536a67dc69f71d6af8f0c0
Benjam1-B <bendu04310@gmail.com>
2022-11-15 23:01:32 +0100

    Projet Portfolio2

--- script.js
+++ script.js
@@ -21,23 +21,3 @@ const handleMousePos = (e) => {
   );
 };
 document.addEventListener('mousemove', handleMousePos);
-
-/*(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('BKcXq_CegrvDvp6gt');
-})();
- 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}*/
