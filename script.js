ef8b375982053cf4308214e34ad140bb3e22c920
Benjam1-B <bendu04310@gmail.com>
2022-11-29 20:01:55 +0100

    Projet PORTFOLIO FIN

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const handleMousePos = (e) => {
-  const CURSOR = document.querySelector('#mouse-cursor');
-  const HOVER = document.querySelectorAll('.cursor-hover');
-  const { pageX: posX, pageY: posY } = e;
+document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+  t.style.left = n.clientX + "px", 
+t.style.top = n.clientY + "px", 
+e.style.left = n.clientX + "px", 
+e.style.top = n.clientY + "px", 
+i.style.left = n.clientX + "px", 
+i.style.top = n.clientY + "px"
+});
+var t = document.getElementById("curseur"),
+  e = document.getElementById("curseur2");
+function n(t) {
+  e.classList.add("survol")
+}
+function s(t) {
+  e.classList.remove("survol")
+}
+s();
+for (var r = document.querySelectorAll(".lien-survol"), a = r.length - 1; a >= 0; a--) {
+  o(r[a])
+}
+function o(t) {
+  t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+}
 
-  const runMouseOver = () => {
-    CURSOR.style.transform = 'scale(3)';
-    CURSOR.style.background = '#ffffff';
-  };
-  HOVER.forEach(hover => hover.addEventListener('mouseenter', runMouseOver));
-
-  const runMouseLeave = () => {
-    CURSOR.style.transform = '';
-    CURSOR.style.background = '';
-  };
-  HOVER.forEach(hover => hover.addEventListener('mouseleave', runMouseLeave));
-  
-  return (
-    CURSOR.style.left = `${posX - 12}px`,
-    CURSOR.style.top = `${posY - 12}px`  
-  );
-};
-document.addEventListener('mousemove', handleMousePos);
+$('.curseur-section').on('mouseover', function(event) {				
+$('body').addClass('bars');
+});
+$('.curseur-section').on('mouseout', function(event) {		
+$('body').removeClass('bars');
+});
